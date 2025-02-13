1b410f457c28e3ba1f73e922aff5d720bc4d0576
Tehapuet <dktv231@gmail.com>
2025-02-14 02:35:31 +0700

    mapV0.1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'into_app.dart';  // นำเข้าไฟล์ IntoApp.dart
 import 'home_page.dart';
 import 'calendar_page.dart';
+import 'map.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),  // กำหนดให้หน้า IntoApp เป็นหน้าเริ่มต้น
+      home: MapDetailPage(),  // กำหนดให้หน้า IntoApp เป็นหน้าเริ่มต้น
     );
   }
 }
